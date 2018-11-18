7165170fc944c8b64abdccfbb9b8a0a1955142e1
janton42 <jantonstock@gmail.com>
2018-11-18 12:37:06 -0800

    Updated settings to collect static assets. Disabled collectstatic step of Heroku build

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+import datetime
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from scheduler.models import 
 	
 def index(request):
 
