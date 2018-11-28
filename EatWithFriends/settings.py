48a77af1b62db85d410baef139348fce4f9f92ac
janton42 <jantonstock@gmail.com>
2018-11-28 07:58:35 -0800

    Set up local mobile dev. a tag 'home' stopped working

--- EatWithFriends/settings.py
+++ EatWithFriends/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.251']
 
 
 # Application definition
